105e8e828a3bc56a9803a375cf3ae1ccff1d8b4d
Zetcode98 <147458187+Zetcode98@users.noreply.github.com>
2023-11-02 06:46:53 +0700

    Add files via upload
    TUGAS PRAKTIKUM PYTHON PEKAN 6

---
+++ tugas3.py
@@ -0,0 +1,14 @@
+string = ""
+bar = 1
+
+x = int (input("masukan angka = "))
+
+while bar <= x:
+    kol = bar
+
+    while kol >= 0:
+        string = string + "*"
+        kol = kol -1
+        string = string + "" 
+        bar = bar +1
+    print (string) 
